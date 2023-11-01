6f4dd916b01d84571059a211c85ca3cd1ea9f5ac
vladisduda <vladisduda@mail.ru>
2023-11-01 17:26:04 +0300

    ''

---
+++ JS/68-79/68.js
@@ -0,0 +1,8 @@
+//№1
+let arr = [1, 2, 3];
+alert(arr);
+console.log(arr);
+//№2
+let arr1 = ['Hallo', 'my name is', 'Gustavo'];
+alert(arr1);
+console.log(arr1);
---
+++ JS/68-79/79.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 3, 4, 5];
+console.log(`Данн массив arr[${arr}]`);
+
+console.log(`№1 Код должен вывести последний элемент массива:`);
+console.log(arr[arr.length - 1]);
+
+console.log(`№2 Код должен найти сумму элементов массива:`);
+console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+
+console.log(`№3 Код должен вывести длину массива:`);
+console.log(arr.length);
+
+console.log(`№4 Код должен вывести длину массива:`);
+console.log(arr.length);
