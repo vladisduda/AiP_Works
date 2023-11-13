01812871b7e6af5dc26621fc4a1502f7488c9470
vladisduda <vladisduda@mail.ru>
2023-11-13 18:27:49 +0300

    ''

---
+++ JS/104-142/105.js
@@ -0,0 +1,31 @@
+//№1
+let test = 11
+if (test > 10) {
+	console.log(`test больше 10, а точнее = ${test}`)
+} else {
+	console.log(`test меньше 10, а точнее = ${test}`)
+}
+
+//№2
+test = 9
+if (test < 10) {
+	console.log(`test меньше 10, а точнее = ${test}`)
+} else {
+	console.log(`test больше 10, а точнее = ${test}`)
+}
+
+//№3
+test = 10
+if (test >= 10) {
+	console.log(`test больше или равна 10, а точнее = ${test}`)
+} else {
+	console.log(`test меньше или равна 10, а точнее = ${test}`)
+}
+
+//№4
+test = 10
+if (test <= 10) {
+	console.log(`test меньше или равна 10, а точнее = ${test}`)
+} else {
+	console.log(`test больше или равна 10, а точнее = ${test}`)
+}
